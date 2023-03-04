3d0e8d90e0d8c01e9dbbd48465716993eb4683fc
Aashman Sharma <90787261+Gammaray09@users.noreply.github.com>
2023-03-04 16:22:15 -0600

    installed firebase

--- App.js
+++ App.js
@@ -1,10 +1,9 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
-import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import LoginScreen from "./screens/LoginScreen";
-import HomeScreen from "./screens/HomeScreen";
+import LoginScreen from "./app/screens/LoginScreen";
+import HomeScreen from "./app/screens/HomeScreen";
 
 const Stack = createNativeStackNavigator();
 
--- app/firebase.js
+++ app/firebase.js
@@ -1,5 +1,5 @@
 // Import the functions you need from the SDKs you need
-import * as firebase from "firebase";
+import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -14,13 +14,4 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-let app;
-if (firebase.apps.length === 0) {
-  app = firebase.initializeApp(firebaseConfig);
-} else {
-  app = firebase.app();
-}
-
-const auth = firebase.auth();
-
-export { auth };
+const app = initializeApp(firebaseConfig);
