f9cb20c67be8d6006db9d2f98e197ad4a48ffc04
EnguerranP <90780255+EnguerranP@users.noreply.github.com>
2023-03-08 10:48:41 -0600

    Added Styles.js, should be working
    The idea is to have a global theme going so that we can be very reliable when it comes

--- app/screens/RecentDataScreen.js
+++ app/screens/RecentDataScreen.js
@@ -1,7 +1,7 @@
 import { useNavigation } from "@react-navigation/core";
 import React from "react";
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
-import { auth } from "../firebase";
+import StyleSheet from '../config/styles';
 
 const HomeScreen = () => {
   const navigation = useNavigation();
@@ -20,9 +20,9 @@ const HomeScreen = () => {
       <Text>Email: {auth.currentUser?.email}</Text>
       <TouchableOpacity
         onPress={handleSignOut}
-        style={styles.button}
+        style={StyleSheet.button}
       >
-        <Text style={styles.buttonText}>Sign out.</Text>
+        <Text style={styles.buttonText}>Sign out</Text>
       </TouchableOpacity>
     </View>
   );
@@ -30,23 +30,3 @@ const HomeScreen = () => {
 
 export default HomeScreen;
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  button: {
-    backgroundColor: "#0782F9",
-    width: "60%",
-    padding: 15,
-    borderRadius: 10,
-    alignItems: "center",
-    marginTop: 40,
-  },
-  buttonText: {
-    color: "white",
-    fontWeight: "700",
-    fontSize: 16,
-  },
-});
