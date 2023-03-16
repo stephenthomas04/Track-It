01ad0b9a41e52ca4866185382249140f1e48ea04
Aashman Sharma <90787261+Gammaray09@users.noreply.github.com>
2023-03-16 10:16:50 -0500

    added data input screen

--- App.js
+++ App.js
@@ -5,20 +5,21 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import LoginScreen from "./app/screens/LoginScreen";
 import HomeScreen from "./app/screens/HomeScreen";
-
-
+import DataInput from "./app/screens/DataInput";
 
 const Stack = createNativeStackNavigator();
-
 
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen options = {{headerShown: false}} name="Login" component={LoginScreen} />
-        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen
+          options={{ headerShown: false }}
+          name="Login"
+          component={LoginScreen}
+        />
+        <Stack.Screen name="DataInput" component={DataInput} />
       </Stack.Navigator>
-  
     </NavigationContainer>
   );
 }
---
+++ app/screens/DataInput.js
@@ -0,0 +1,84 @@
+import React, { useState } from "react";
+import { StyleSheet, View, TextInput, Button } from "react-native";
+
+const DataInput = () => {
+  const [storeName, setStoreName] = useState("");
+  const [totalPrice, setTotalPrice] = useState("");
+  const [address, setAddress] = useState("");
+  const [date, setDate] = useState("");
+  const [image, setImage] = useState("");
+
+  const handleInput1Change = (text) => {
+    setStoreName(text);
+  };
+
+  const handleInput2Change = (text) => {
+    setTotalPrice(text);
+  };
+
+  const handleInput3Change = (text) => {
+    setAddress(text);
+  };
+
+  const handleInput4Change = (text) => {
+    setDate(text);
+  };
+
+  const handleInput5Change = (text) => {
+    setImage(text);
+  };
+
+  const handlePress = () => {
+    console.log(setStoreName, setTotalPrice, setAddress, setDate);
+  };
+
+  return (
+    <View style={styles.container}>
+      <TextInput
+        style={styles.input}
+        onChangeText={handleInput1Change}
+        value={setStoreName}
+        placeholder="Store"
+      />
+      <TextInput
+        style={styles.input}
+        onChangeText={handleInput2Change}
+        value={setTotalPrice}
+        placeholder="Price"
+      />
+      <TextInput
+        style={styles.input}
+        onChangeText={handleInput3Change}
+        value={setAddress}
+        placeholder="Address"
+      />
+      <TextInput
+        style={styles.input}
+        onChangeText={handleInput4Change}
+        value={setDate}
+        placeholder="Date"
+      />
+      <Button title="Submit" onPress={handlePress} />
+    </View>
+  );
+};
+
+export default DataInput;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#fff",
+    padding: 20,
+  },
+  input: {
+    height: 40,
+    width: "100%",
+    borderColor: "gray",
+    borderWidth: 1,
+    marginBottom: 20,
+    paddingHorizontal: 10,
+  },
+});
