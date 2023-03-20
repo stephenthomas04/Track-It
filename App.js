6768901eae5c1c918568aff84157fb175aa9f4d3
Stephen Thomas <80729552+stephenthomas04@users.noreply.github.com>
2023-03-20 10:58:59 -0500

    improved UI

--- App.js
+++ App.js
@@ -9,6 +9,7 @@ import { createDrawerNavigator } from "@react-navigation/drawer";
 import { NavigationContainer } from "@react-navigation/native";
 import GraphScreen from "./app/screens/GraphScreen";
 import DataInput from "./app/screens/DataInput";
+import { FontAwesome } from "@expo/vector-icons";
 
 const Stack = createNativeStackNavigator();
 
@@ -32,13 +33,60 @@ function App() {
 
 function RouteName() {
   return (
-      <Drawer.Navigator initialRouteName="Home">
-        <Drawer.Screen options={{ headerShown: false }}  name="Home" component={HomeScreen} />
-        <Drawer.Screen name="GraphScreen" component={GraphScreen} />
-        <Drawer.Screen name="DataInput" component={DataInput} />
-        <Drawer.Screen name="Settings" component={Settings} />
-      </Drawer.Navigator>
-  );
+    <Drawer.Navigator
+      initialRouteName="Home"
+      screenOptions={{
+        drawerType: "front",
+        drawerPosition: "left",
+        swipeEdgeWidth: 500,
+        drawerHideStatusBarOnOpen: true,
+        headerShown: true,
+
+        drawerStyle: {
+          backgroundColor: "#c6cbef",
+          width: 240,
+        },
+      }}
+    >
+      <Drawer.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          drawerIcon: () => (
+            <FontAwesome name="home" size={24} color="black" />
+          ),
+        }}
+      />
+      <Drawer.Screen
+        name="GraphScreen"
+        component={GraphScreen}
+        options={{
+          drawerIcon: () => (
+            <FontAwesome name="gear" size={24} color="black" />
+          ),
+        }}
+      />
+      <Drawer.Screen
+        name="DataInput"
+        component={DataInput}
+        options={{
+          drawerIcon: () => (
+            <FontAwesome name="user" size={24} color="black" />
+          ),
+        }}
+      />
+      <Drawer.Screen
+        name="Settings"
+        component={Settings}
+        options={{
+          drawerIcon: () => (
+            <FontAwesome name="info-circle" size={24} color = "tomato" />
+          ),
+        }}
+      />
+      
+    </Drawer.Navigator>
+);
 
 
 
