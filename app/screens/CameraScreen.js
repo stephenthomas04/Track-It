91de098a4c8488c9b4dcf8b9924605acf15af21c
Aashman Sharma <90787261+Gammaray09@users.noreply.github.com>
2023-04-03 23:14:07 -0500

    added the capability to take/retake pictures with the camera

--- app/screens/CameraScreen.js
+++ app/screens/CameraScreen.js
@@ -1,14 +1,115 @@
-import React, { useState } from "react";
-import { StyleSheet, View, TextInput, Button, Text } from "react-native";
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, View, TouchableOpacity, Image } from "react-native";
+import { Camera } from "expo-camera";
 
 import globalStyle from "../config/globalStyle";
 
 function CameraScreen() {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [cameraRef, setCameraRef] = useState(null);
+  const [photo, setPhoto] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestPermissionsAsync();
+      setHasPermission(status === "granted");
+    })();
+  }, []);
+
+  const takePicture = async () => {
+    if (cameraRef) {
+      const options = { quality: 0.5, base64: true };
+      const data = await cameraRef.takePictureAsync(options);
+      setPhoto(data.uri);
+    }
+  };
+
+  const retakePicture = () => {
+    setPhoto(null);
+  };
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
   return (
-    <View>
-      <Text>Camera Screen</Text>
+    <View style={styles.container}>
+      {photo ? (
+        <View style={styles.preview}>
+          <Text style={styles.previewText}>Receipt Image:</Text>
+          <Image style={styles.previewImage} source={{ uri: photo }} />
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => retakePicture()}
+          >
+            <Text style={styles.buttonText}>Retake Image</Text>
+          </TouchableOpacity>
+        </View>
+      ) : (
+        <Camera style={styles.camera} ref={(ref) => setCameraRef(ref)}>
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity
+              style={styles.button}
+              onPress={() => takePicture()}
+            >
+              <Text style={styles.buttonText}>Take Picture</Text>
+            </TouchableOpacity>
+          </View>
+        </Camera>
+      )}
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF",
+  },
+  camera: {
+    flex: 1,
+    justifyContent: "flex-end",
+    alignItems: "center",
+    width: "100%",
+  },
+  buttonContainer: {
+    flex: 0,
+    flexDirection: "row",
+    justifyContent: "center",
+    marginBottom: 20,
+  },
+  button: {
+    width: 100,
+    height: 50,
+    backgroundColor: "#222",
+    borderRadius: 10,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 16,
+  },
+  preview: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF",
+  },
+  previewText: {
+    fontSize: 30,
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
+  previewImage: {
+    width: 300,
+    height: 600,
+    borderRadius: 10,
+  },
+});
 
 export default CameraScreen;
